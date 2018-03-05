e84ff7f6fe5f7b4a15aff1a88bc1c7b848fe2896
QianguoSun <1321377436@qq.com>
2018-03-05 21:02:23 +0800

    update1

--- src/DP_algorithm.java
+++ src/DP_algorithm.java
@@ -92,6 +92,46 @@ public class DP_algorithm {
 
     }
     //=================动态规划第三题=====================================//
+    /**
+     * 话说有一哥们去森林里玩发现了一堆宝石，他数了数，
+     * 一共有n个。 但他身上能装宝石的就只有一个背包，背包的容量为C。
+     * 这哥们把n个宝石排成一排并编上号： 0,1,2,…,n-1。
+     * 第i个宝石对应的体积和价值分别为V[i]和W[i] 。排好后这哥们开始思考：
+     * 背包总共也就只能装下体积为C的东西，那我要装下哪些宝石
+     * 才能让我获得最大的利益呢？*/
+    //solution1 暴力搜索算法
+    static int n=10;
+    static int W;
+    static int[] w = new int[n];
+    static int[] v = new int[n];
+    static int search(int idx, int s){
+        if(s>W){
+            return 0;
+        }
+        if(idx>=n){
+            return 0;
+        }
+
+        return Math.max(search(idx+1,s+w[idx])+v[idx],
+                search(idx+1,s));
+    }
+    //solution2 保存已经计算过的变量
+    static int[][] f = new int[n][n];
+    static int searchs(int idx, int s){
+        if(s>W){
+            return 0;
+        }
+        if(idx>=n){
+            return 0;
+        }
+        if(f[idx][s]>0){
+            return f[idx][s];
+        }
+
+        f[idx][s] = Math.max(searchs(idx+1,s+w[idx])+v[idx],
+                searchs(idx+1,s));
+        return f[idx][s];
+    }
 
     public static void main(String[] args){
 
