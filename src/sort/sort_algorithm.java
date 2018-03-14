196dc8feaad089c327b98f8bacef216830462d22
QianguoSun <1321377436@qq.com>
2018-03-14 21:21:01 +0800

    update

--- src/sort/sort_algorithm.java
+++ src/sort/sort_algorithm.java
@@ -8,17 +8,17 @@ public class sort_algorithm {
     public static int[] nums = new int[]{122,87,78,45,17,65,53,9,32};
 
     //冒泡排序算法
-    public static void maopao_sort(){
-        int n = nums.length;
+    public static void maopao_sort(int[] a){
+        int n = a.length;
         for(int i=n-1;i>=0;i--){
             for(int j=0;j<i;j++){
-                if(nums[j]>nums[j+1]){
-                    swap(j,j+1);
+                if(a[j]>a[j+1]){
+                    swap(a,j,j+1);
                 }
             }
         }
         for(int i=0;i<n;i++){
-            System.out.print(nums[i]+" ");
+            System.out.print(a[i]+" ");
         }
     }
     private static void swap(int i, int j) {
@@ -27,84 +27,84 @@ public class sort_algorithm {
         nums[j] = temp;
     }
     //插入排序算法
-    public static void insert_sort(){
-        int n = nums.length;
+    public static void insert_sort(int[] a){
+        int n = a.length;
         int i;
         int j;
         for(i=1;i<n;i++){
-            int temp = nums[i];
+            int temp = a[i];
             for(j=i-1;j>=0;j--){
-                if(nums[j]>temp){
-                    nums[j+1] = nums[j];
+                if(a[j]>temp){
+                    a[j+1] = a[j];
                 }else{
                     break;
                 }
             }
-            nums[++j] = temp;
+            a[++j] = temp;
         }
         for(int st=0;st<n;st++){
-            System.out.print(nums[st]+" ");
+            System.out.print(a[st]+" ");
         }
     }
     //选择排序算法
-    public static void select_sort(){
-        int n = nums.length;
+    public static void select_sort(int[] a){
+        int n = a.length;
         for(int i=0;i<n-1;i++){
             int index = i;
             for(int j=i+1;j<n;j++){
-                if(nums[j]<nums[index]){
+                if(a[j]<a[index]){
                     index = j;
                 }
             }
             if(index!=i){
-                swap(i,index);
+                swap(a,i,index);
             }
         }
         for(int st=0;st<n;st++){
-            System.out.print(nums[st]+" ");
+            System.out.print(a[st]+" ");
         }
     }
     //快速排序
-    public static void quick_sort(){
-        for(int st=0;st<nums.length;st++){
-            System.out.print(nums[st]+" ");
+    public static void quick_sort(int[] a){
+        for(int st=0;st<a.length;st++){
+            System.out.print(a[st]+" ");
         }
         System.out.println();
-        recquick_sort(0,nums.length-1);
-        for(int st=0;st<nums.length;st++){
-            System.out.print(nums[st]+" ");
+        recquick_sort(a,0,a.length-1);
+        for(int st=0;st<a.length;st++){
+            System.out.print(a[st]+" ");
         }
     }
-    public static void recquick_sort(int left,int right){
+    public static void recquick_sort(int[] a,int left,int right){
         if(left>=right){
             return;
         }else{
-            int partion = getPartions(left,right);
-            recquick_sort(left,partion-1);
-            recquick_sort(partion+1,right);
+            int partion = getPartions(a,left,right);
+            recquick_sort(a,left,partion-1);
+            recquick_sort(a,partion+1,right);
         }
     }
 
-    private static int getPartions(int left, int right) {
+    private static int getPartions(int[] a,int left, int right) {
         int leftPtr = left;
         int rightPtr = right-1;
         while(true){
-            while(leftPtr<right && nums[leftPtr]<=nums[right]) leftPtr++;
-            while(rightPtr>=0 && nums[rightPtr]>nums[right])rightPtr--;
+            while(leftPtr<right && a[leftPtr]<=a[right]) leftPtr++;
+            while(rightPtr>=0 && a[rightPtr]>a[right])rightPtr--;
             if(leftPtr<rightPtr){
-                swap(leftPtr,rightPtr);
+                swap(a,leftPtr,rightPtr);
             }else{
                 break;
             }
         }
-        swap(leftPtr,right);
+        swap(a,leftPtr,right);
         return leftPtr;
 
 
     }
     //希尔排序
-    public static void shell_sort(){
-        int n = nums.length;
+    public static void shell_sort(int[] a){
+        int n = a.length;
         int h=1;
         int out,inner;
         while(h<=(n/3)){
@@ -112,58 +112,79 @@ public class sort_algorithm {
         }
         while(h>0){
             for(out=h;out<n;out++){
-                int temp = nums[out];
+                int temp = a[out];
                 inner = out;
-                while (inner>h-1 && nums[inner-h] > temp){
-                    nums[inner] = nums[inner-h];
+                while (inner>h-1 && a[inner-h] > temp){
+                    a[inner] = a[inner-h];
                     inner -= h;
                 }
-                nums[inner] = temp;
+                a[inner] = temp;
             }
             h = (h-1)/3;
         }
-        for(int st=0;st<nums.length;st++){
-            System.out.print(nums[st]+" ");
+        for(int st=0;st<a.length;st++){
+            System.out.print(a[st]+" ");
         }
 
     }
 
-    //堆排序
-    public static void heap_sort(){
-        int length = nums.length;
+    //堆排序具体代码详细
+
+    public static void heap_sort(int[] a){
+        if(a.length == 0) return;
+        int length = a.length;
+        buildHeap(a);
         for(int i=length-1;i>0;i--){
-            swap(i,0);
-            sink(0,i);
+            swap(a,i,0);
+            sink(a,0,i);
         }
-        for(int st=0;st<nums.length;st++){
-            System.out.print(nums[st]+" ");
+        for(int st=0;st<a.length;st++){
+            System.out.print(a[st]+" ");
         }
     }
 
-    private static void sink(int k, int len) {
-        int temp = nums[0];
-        for(int i = 2*k+1;i<len;i=2*k+1){
-            if(i<len-1 && nums[i]<nums[i+1]){
-                i++;
+    private static void buildHeap(int[] a) {
+        for(int i = a.length/2-1;i>=0;i--){
+            sink(a,i,a.length);
+        }
+    }
+
+    private static void sink(int[] a, int i, int length) {
+        while(i<length){
+            int child = getLeftChild(i);
+            if(child>=length) break;
+            if(child+1<length && a[child]<a[child+1]){
+                child++;
             }
-            if(temp>nums[i]){
-                break;
+            if(a[child]>a[i]){
+                swap(a,child,i);
+                i = child;
             }else{
-                nums[k] = nums[i];
-                k = i;
-
+                break;
             }
         }
-        nums[k] = temp;
     }
+
+    private static void swap(int[] a, int child, int i) {
+        int temp = a[child];
+        a[child] = a[i];
+        a[i] = temp;
+    }
+
+    private static int getLeftChild(int i) {
+        return 2*i+1;
+    }
+
+
 
     public static void main(String[] args){
-        //maopao_sort();
-        //insert_sort();
-        //select_sort();
-        //quick_sort();
-        //shell_sort();//{5,2,6,3,7,8,9,1}
-        heap_sort();
+        int[] a = new int[]{2,6,1,9,3,5,4};
+        //maopao_sort(a);
+        //insert_sort(a);
+        //select_sort(a);
+        //quick_sort(a);
+        //shell_sort(a);//{5,2,6,3,7,8,9,1}
+        //heap_sort(a);
 
     }
 }
